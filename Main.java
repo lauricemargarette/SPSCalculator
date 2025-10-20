01a630b3065e1d525c2f33006d67ad2be4e0ff8c
Laurice Margarette Pates <lauricepates1126@gmail.com>
2025-10-20 10:59:27 +0800

    Executing changes.

--- Main.java
+++ Main.java
@@ -1,74 +1,9 @@
+import javax.swing.*;
 import java.util.Scanner;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.*;
-import javax.swing.JLabel;
-
-public class Main extends JFrame{
-    final private Font mainFont = new Font("Poppins", Font.BOLD, 18);
-    JTextField billerName, billerAddress, billerContact;
-    JLabel welcomeText;
-
-    public void initialize() {
-        /*FORMS PANEL*/
-        JLabel billerFullName = new JLabel("Biller's Name: ");
-        billerFullName.setFont(mainFont);
-
-        billerName = new JTextField();
-        billerName.setFont(mainFont);
-
-        JPanel formPanel = new JPanel();
-        formPanel.setLayout(new GridLayout(4, 1, 5, 5));
-        formPanel.add(billerFullName);
-        formPanel.add(billerName);
-
-        /*WELCOME TEXT*/
-        welcomeText = new JLabel();
-        welcomeText.setFont(mainFont);
-
-        /*Buttons Panel*/
-        JButton buttons = new JButton("NEXT");
-        buttons.setFont(mainFont);
-        buttons.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String billerFullName = billerName.getText();
-                welcomeText.setText("Hello, " + billerFullName + "!");
-            }
-        });
-
-        JButton buttonsClr = new JButton("CLEAR");
-        buttonsClr.setFont(mainFont);
-        buttonsClr.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                billerName.setText("");
-                welcomeText.setText("");
-            }
-        });
-
-        JPanel buttonsPanel = new JPanel();
-        buttonsPanel.setLayout(new GridLayout(1, 2, 5, 5));
-        buttonsPanel.add(buttons);
-        buttonsPanel.add(buttonsClr);
-
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout());
-        mainPanel.setBackground(new Color(255, 255, 250));
-        mainPanel.add(formPanel, BorderLayout.NORTH);
-        mainPanel.add(welcomeText, BorderLayout.CENTER);
-        mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
-
-        add(mainPanel);
-
-        setTitle("Welcome");
-        setSize(500, 600);
-        setMinimumSize(new Dimension(600, 400));
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setVisible(true);
-    }
+public class Main {
     public static void main(String[] args) {
+        
         Scanner input = new Scanner(System.in);
 
         Appliances appliance = new Appliances();
@@ -144,8 +79,11 @@ public class Main extends JFrame{
                 appliance.calculateTotalWattHours() - solarBattery.getPower());
         }
 
-        Main myFrame = new Main();
-        myFrame.initialize();
+    SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new MainFrame(); // Create and show your GUI
+                }
+        });
     }
 
     public static String input(String message, Scanner input) {
---
+++ MainFrame.java
@@ -0,0 +1,82 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame{
+    final private Font mainFont = new Font("Poppins", Font.BOLD, 18);
+    JTextField billerName, billerAddress, billerContact;
+    JLabel welcomeText;
+
+    public void initialize() {
+        /*FORMS PANEL*/
+        JLabel billerFullName = new JLabel("Biller's Name: ");
+        billerFullName.setFont(mainFont);
+
+        billerName = new JTextField();
+        billerName.setFont(mainFont);
+
+        JPanel formPanel = new JPanel();
+        formPanel.setLayout(new GridLayout(4, 1, 5, 5));
+        formPanel.add(billerFullName);
+        formPanel.add(billerName);
+
+        /*WELCOME TEXT*/
+        welcomeText = new JLabel();
+        welcomeText.setFont(mainFont);
+
+        /*Buttons Panel*/
+        JButton buttons = new JButton("NEXT");
+        buttons.setFont(mainFont);
+        buttons.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String billerFullName = billerName.getText();
+                welcomeText.setText("Hello, " + billerFullName + "!");
+            }
+        });
+
+        JButton buttonsClr = new JButton("CLEAR");
+        buttonsClr.setFont(mainFont);
+        buttonsClr.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                billerName.setText("");
+                welcomeText.setText("");
+            }
+        });
+
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new GridLayout(1, 2, 5, 5));
+        buttonsPanel.add(buttons);
+        buttonsPanel.add(buttonsClr);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.setBackground(new Color(255, 255, 250));
+        mainPanel.add(formPanel, BorderLayout.NORTH);
+        mainPanel.add(welcomeText, BorderLayout.CENTER);
+        mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
+
+        add(mainPanel);
+
+        setTitle("Welcome");
+        setSize(500, 600);
+        setMinimumSize(new Dimension(600, 400));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JLabel label = new JLabel("Welcome to my app!");
+        add(label);
+
+        setVisible(true);
+    }
+
+    public void performSomeAction() {
+        System.out.println("Action performed!");
+    }
+
+    public static void main(String[] args) {
+    MainFrame myFrame = new MainFrame();
+    myFrame.initialize();
+}
+}
